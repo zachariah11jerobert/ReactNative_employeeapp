9b2e4cc866eaf41320a6ae4354b4553600dd7a42
unknown <zachariah11jerobert@gmail.com>
2021-08-14 15:24:21 +0530

    multiple cards were displayed using map with dummy data

--- screens/Home.js
+++ screens/Home.js
@@ -3,19 +3,34 @@ import { StyleSheet, Text, View,Image } from 'react-native';
 import {Card} from 'react-native-paper';
 
 const Home=()=>{
-    return(
-        <Card style={styles.maycard}>
+    const items=[
+        {id:1,name:"mukesh",position:"web dev"},
+        {id:2,name:"suresh",position:"android dev"},
+        {id:3,name:"ramesh",position:"ML expert"},
+        {id:4,name:"hitesh",position:"web dev"},
+    ]
+
+    const renderList = items.map((item)=>{
+        return( 
+        <Card style={styles.maycard} key={item.id}>
             <View style={styles.cardView}>
                 <Image
                 style={{width:60,height:60,borderRadius:30}}
                 source={{uri:"https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"}}
                 />
                 <View style={{marginLeft:10}}>
-                    <Text style={styles.text}>Hello from Home</Text>
-                    <Text style={styles.text}>Web developer</Text>
+                    <Text style={styles.text}>{item.name}</Text>
+                    <Text style={styles.text}>{item.position}</Text>
                 </View>
             </View>
         </Card>
+        );
+    })
+
+    return(
+       <View>
+         {renderList}  
+       </View>
     )
 }
 
