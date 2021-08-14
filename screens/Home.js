2cb7553df41ca0a32244e4bf89fe9ea550366eb4
unknown <zachariah11jerobert@gmail.com>
2021-08-14 15:14:27 +0530

    created a simple card component with image & text

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 import {Card} from 'react-native-paper';
 
 const Home=()=>{
     return(
         <Card style={styles.maycard}>
-        <Text style={{fontSize:22}}>Hello from Home</Text>
+            <View style={styles.cardView}>
+                <Image
+                style={{width:60,height:60,borderRadius:30}}
+                source={{uri:"https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"}}
+                />
+                <View style={{marginLeft:10}}>
+                    <Text style={styles.text}>Hello from Home</Text>
+                    <Text style={styles.text}>Web developer</Text>
+                </View>
+            </View>
         </Card>
     )
 }
@@ -13,7 +22,13 @@ const Home=()=>{
 const styles=StyleSheet.create({
     maycard:{
         margin:5,
-        padding:5
+    },
+    cardView:{
+        flexDirection:"row",
+        padding:6
+    },
+    text:{
+        fontSize:18,
     }
 })
 
