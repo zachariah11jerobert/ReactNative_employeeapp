448c7f4b2d1e3278cd4b91d14ba8792b7d7c93a9
unknown <zachariah11jerobert@gmail.com>
2021-08-17 19:16:59 +0530

    profile part 2

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { Title, Card, Button } from "react-native-paper";
+import { MaterialIcons, Entypo } from "@expo/vector-icons";
 
 const Profile = () => {
   return (
@@ -11,19 +13,88 @@ const Profile = () => {
       />
       <View style={{ alignItems: "center" }}>
         <Image
-          style={{ width: 140, height: 140, borderRadius: 140/2, marginTop: -50 }}
+          style={{
+            width: 140,
+            height: 140,
+            borderRadius: 140 / 2,
+            marginTop: -50,
+          }}
           source={{
             uri: "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
           }}
         />
       </View>
+      <View style={{ alignItems: "center", margin: 15 }}>
+        <Title>John Zachary</Title>
+        <Text style={{ fontSize: 15 }}>Web Developer</Text>
+      </View>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name="email" size={32} color="#006aff" />
+          <Text style={styles.mytext}>abc@abc.com</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <Entypo name="phone" size={32} color="#006aff" />
+          <Text style={styles.mytext}>9477854562</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name="attach-money" size={32} color="#006aff" />
+          <Text style={styles.mytext}>8 LPA</Text>
+        </View>
+      </Card>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          padding: 5,
+        }}
+      >
+        <Button
+          icon="account-edit"
+          mode="contained"
+          theme={theme}
+          onPress={() => console.log("Pressed")}
+        >
+          Edit
+        </Button>
+        <Button
+          icon="delete"
+          mode="contained"
+          theme={theme}
+          onPress={() => console.log("Pressed")}
+        >
+          Fire employee
+        </Button>
+      </View>
     </View>
   );
+};
+
+const theme = {
+  colors: {
+    primary: "#006aff",
+  },
 };
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  mycard: {
+    margin: 3,
+  },
+  cardContent: {
+    flexDirection: "row",
+    padding: 8,
+  },
+  mytext: {
+    fontSize: 18,
+    marginTop: 3,
+    marginLeft: 5,
   },
 });
 
